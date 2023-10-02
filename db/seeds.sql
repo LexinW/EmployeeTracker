b26a3dbb7eb0207d45ca8e3fcc6d59052ebaa25e
User <lexin_wiesner@yahoo.com>
2023-10-02 14:03:00 -0700

    Created the seed file and added inquirer functionality

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,2 +1,11 @@
 
+INSERT INTO department (name) VALUES
+  ('Software'),
+  ('Sales'),
+  ('Engineering');
+
+INSERT INTO role (title, salary, department_id) VALUES
+  ('Junior Dev', 60000, 1),
+  ('Sales Representative', 50000, 2),
+  ('Engineer', 80000, 3);
        
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
-// Import and require mysql2
 const mysql = require('mysql2');
+const inquirer = require('inquirer');
 //added dotenv to create password file, more secure for github.
 require('dotenv').config();
 
@@ -16,11 +16,72 @@ const db = mysql.createConnection(
   {
     host: 'localhost',
     user: 'root',
-    password: process.env.DB_PASSWORD,
+    password: '9498',
     database: 'employee_db'
   },
-  console.log(`Connected to the movies_db database.`)
+  mainMenu(),
+  console.log(`Connected to the employee_db database.`)
 );
+async function mainMenu() {
+  const { choice } = await inquirer.prompt([
+    {
+      type: 'list',
+      name: 'choice',
+      message: 'Select an option:',
+      choices: [
+        'View all departments',
+        'View all roles',
+        'View all employees',
+        'Add a department',
+        'Add a role',
+        'Add an employee',
+        'Update an employee role',
+        'Exit',
+      ],
+    },
+  ]);
+
+  switch (choice) {
+    case 'View all departments':
+      viewAllDepartments();
+      break;
+    case 'View all roles':
+      // Call a function to view all roles
+      break;
+    case 'View all employees':
+      // Call a function to view all employees
+      break;
+    case 'Add a department':
+      // Call a function to add a department
+      break;
+    case 'Add a role':
+      // Call a function to add a role
+      break;
+    case 'Add an employee':
+      // Call a function to add an employee
+      break;
+    case 'Update an employee role':
+      // Call a function to update an employee's role
+      break;
+    case 'Exit':
+      console.log('Goodbye!');
+      process.exit(0);
+      break;
+    default:
+      console.log('Invalid choice');
+  }
+}
+function viewAllDepartments() {
+  const query = 'SELECT * FROM department';
+  db.query(query, (err, results) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.table(results);
+    mainMenu();
+  });
+}
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
